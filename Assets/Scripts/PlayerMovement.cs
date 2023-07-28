667d2f066052c8f896ab03b412ef837b62676835
gturksever <gsgturk@hotmail.com>
2023-07-28 19:00:09 +0300

    Second commit
    Added collectible/finishLine/GameInstance/obstacle scripts

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,25 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float _forwardSpeed;
-    [SerializeField] private float _horizontalSpeed;
+	[SerializeField] private float _forwardSpeed;
 
-    private void Update()
-    {
-        Vector3 velocity =  new Vector3();
+	[SerializeField] private float _horizontalSpeed;
 
+	[SerializeField] private Rigidbody _rigidbody;
+	
+    private Vector3 _velocity = new Vector3();
 
-        velocity.z = _forwardSpeed;
-        velocity.x = Input.GetAxis("Horizontal") * _horizontalSpeed; // sol sað için -1 ile 1 arasýnda float deðiþken tutar yumuþak geçiþ ayarýný imput managerden ayarlayabiliriz.
+	private void Start()
+	{
+	}
 
-        transform.position += velocity*Time.deltaTime;
-        
-        
-        //Transform tr = GetComponent<Transform>(); // transform.blabla = GetComponent<Transform>() transform için kolay bir kullaným unity bize saðlýyor.
-        //transform.position ile localposition arasýndaki fark position dünyaya göre olan konumu localposition parenti ile arasýndaki kontrol. eðer bir objenin parenti yoksa ikisi ayný þey.
-    }//euler angles xyz den oluþan bir açý temsil eder. euler angleslarýn sýkýntýlarý olabiliyor o yüzden quaternionlarý çok karmaþýk matematiksel iþlemler için bazen kullanmayý tercih ediyoruz. 
+	private void Update()
+	{
+		_velocity.z = _forwardSpeed;
+		_velocity.y = _rigidbody.velocity.y;
+		_velocity.x = Input.GetAxis("Horizontal") * _horizontalSpeed;
+	}
+
+	private void FixedUpdate()
+	{
+		_rigidbody.velocity = _velocity;
+	}
 }
